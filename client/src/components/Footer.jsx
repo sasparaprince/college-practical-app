68f0fd45e1a9acf3cb5859882e59866019ff7267
prince saspara <princesaspara46@gmail.com>
2023-10-01 11:43:06 +0530

    footer update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <div className="container mx-auto">
         <div>
           <p className="text-sm">
-            &copy; {currentYear} <a herf= 'https://college-practicals.vercel.app/'> College Practical Hub </a>
+            &copy; {currentYear} <a href="https://college-practicals.vercel.app/">College Practical Hub</a>
           </p>
         </div>
         <div>
