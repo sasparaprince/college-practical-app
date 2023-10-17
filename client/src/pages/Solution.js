d9b0364c3e3b94215aec4a5979eb7a95ebded6d0
prince saspara <princesaspara46@gmail.com>
2023-10-17 12:49:51 +0530

    output black and white button added

--- client/src/pages/Solution.js
+++ client/src/pages/Solution.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { prism } from 'react-syntax-highlighter/dist/esm/styles/prism';
 import Navbar from '../components/Navbar';
@@ -9,22 +9,27 @@ import Loader from '../components/Spinner';
 import NotFound from '../components/NotFound';
 import { Helmet } from "react-helmet";
 
+import { faMoon } from '@fortawesome/free-solid-svg-icons'
+import { faSun } from '@fortawesome/free-solid-svg-icons'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
 const Solution = () => {
   const { practicalId } = useParams();
-  const [practical, setPractical] = useState(null); // Add state for practical
+  const [practical, setPractical] = useState(null);
   const [solutions, setSolutions] = useState([]);
   const [isCopied, setIsCopied] = useState(false);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [outputBackgroundBlack, setOutputBackgroundBlack] = useState(false);
 
   useEffect(() => {
     axios
       .get(`https://college-practical.vercel.app/api/practicals/${practicalId}/solutions`)
       .then((response) => {
-        setPractical(response.data); // Set the practical data
-        setSolutions(response.data.solutions); // Set the solutions data
+        setPractical(response.data);
+        setSolutions(response.data.solutions);
         setLoading(false);
       })
       .catch((error) => {
@@ -49,6 +54,10 @@ const Solution = () => {
     }, 2000);
   };
 
+  const toggleOutputBackgroundColor = () => {
+    setOutputBackgroundBlack(!outputBackgroundBlack);
+  };
+
   if (loading) {
     return <Loader />
   }
@@ -63,52 +72,66 @@ const Solution = () => {
 
   return (
     <>
-            <Helmet>
-                <meta charSet="utf-8" />
-                <title>{practical.aim}</title>
-                <meta name='description' content={practical.aim}/>
-
-            </Helmet>
-    <Navbar />
-    <div className="container mx-auto p-4">
-      <h2 className="text-2xl text-white font-semibold mb-4">
-        Solutions for Practical {}
-      </h2>
-      <div className='bg-white p-4 rounded-lg shadow-md mb-4'>
-      <h3 className="text-lg font-medium mb-4">Practical Aim: {practical.aim}</h3>
-
-      </div>
-      {solutions.map((solution) => (
-        <div key={solution._id} className="bg-white p-4 rounded-lg shadow-md mb-4">
-          <div className="bg-gray-100 p-4 rounded-lg">
-            <strong>Code:</strong>
-            <div className="relative">
-              <button
-                onClick={() => copyCodeToClipboard(solution.solutionCode)}
-                className={`absolute top-2 right-2 bg-blue-500 text-white p-1 rounded cursor-pointer ${
-                  isCopied ? 'bg-green-500' : ''
-                }`}
-              >
-                {isCopied ? 'Copied!' : 'Copy'}
-              </button>
-              <SyntaxHighlighter language="c" style={prism}>
-                {solution.solutionCode}
-              </SyntaxHighlighter>
-            </div>
-          </div>
-          <div className="mt-4">
-            <strong>Output:</strong>
-            <pre className="bg-white p-4 border rounded-lg whitespace-pre-wrap">{solution.codeOutput}</pre>
-          </div>
-          <div className="mt-4">
-            <strong>Explanation:</strong>
-            <pre className="bg-gray-300 p-4 rounded-lg whitespace-pre-wrap">{solution.explanation}</pre>
-          </div>
-        
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>{practical.aim}</title>
+        <meta name='description' content={practical.aim} />
+      </Helmet>
+      <Navbar />
+      <div className="container mx-auto p-4">
+        <h2 className="text-2xl text-white font-semibold mb-4">
+          Solutions for Practical {}
+        </h2>
+        <div className='bg-white p-4 rounded-lg shadow-md mb-4'>
+          <h3 className="text-lg font-medium mb-4">Practical Aim: {practical.aim}</h3>
         </div>
-      ))}
-    </div>
-    <Footer />
+        {solutions.map((solution) => (
+          <div key={solution._id} className="bg-white p-4 rounded-lg shadow-md mb-4">
+            <div className="bg-gray-100 p-4 rounded-lg">
+              <strong>Code:</strong>
+              <div className="relative">
+                <button
+                  onClick={() => copyCodeToClipboard(solution.solutionCode)}
+                  className={`absolute top-2 right-2 bg-blue-500 text-white p-1 rounded cursor-pointer ${
+                    isCopied ? 'bg-green-500' : ''
+                  }`}
+                >
+                  {isCopied ? 'Copied!' : 'Copy'}
+                </button>
+                <SyntaxHighlighter language="c" style={prism}>
+                  {solution.solutionCode}
+                </SyntaxHighlighter>
+              </div>
+            </div>
+
+
+            <div className='relative'>
+
+     
+            <strong>Output:</strong>
+            <button onClick={toggleOutputBackgroundColor}
+                 className='absolute top-2 right-2 p-1 rounded cursor-pointer icon fa-xl'
+                 
+                >
+                {outputBackgroundBlack ? <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}
+                
+
+                </button>
+                </div>
+
+            <div className={`mt-4 ${outputBackgroundBlack ? 'bg-black text-white' : 'bg-white'}`}>
+              <pre className="p-4 border rounded-lg whitespace-pre-wrap">{solution.codeOutput}</pre>
+            </div>
+
+            <div className="mt-4">
+              <strong>Explanation:</strong>
+              <pre className="bg-gray-300 p-4 rounded-lg whitespace-pre-wrap">{solution.explanation}</pre>
+            </div>
+      
+          </div>
+        ))}
+      </div>
+      <Footer />
     </>
   );
 };
