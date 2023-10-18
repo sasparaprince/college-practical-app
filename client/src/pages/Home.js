1cf160de6e81db6aece1df30fa8399e3bf1d1ac1
prince saspara <princesaspara46@gmail.com>
2023-10-18 16:06:41 +0530

    update earth model

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import { Helmet } from 'react-helmet';
-import Earth from '../components/Earth'; // Import the Earth component
+import Earth from '../components/canvas/Earth'; // Import the Earth component
 import '../App.css'
 
 
@@ -28,29 +28,29 @@ const Home = () => {
             </header>
 
             <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-16 sm:gap-y-20">
-                    <div className="order-2 md:order-1">
-                        <div className="p-4 pl-0 md:p-10 md:mt-10 text-center md:text-left">
-                            <h1 className="text-4xl font-bold text-white ">
-                                Practical Solutions for Every <span className="text-[#71C9CE] font-extrabold drop-shadow-lg">Student</span>
-                            </h1>
-                            <Link to={'/subject'}>
-                                <button className="bg-white mt-5 hover:bg-gray-500 hover:text-white shadow-lg shadow-slate-700 text-gray-800 font-semibold w-[10rem] h-[3.4rem] py-2 px-4 border border-gray-400  rounded-xl">
-                                    Subjects
-                                </button>
-                            </Link>
-                        </div>
-                    </div>
+  <div className="grid grid-cols-1 md:grid-cols-2 h-[32rem] md:h-[25rem] gap-4 md:gap-8 lg:gap-12 xl:gap-16">
+    <div className="order-2 md:order-1">
+      <div className="p-4 md:p-10 md:mt-10 text-center md:text-left">
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-white">
+          Practical Solutions for Every{" "}
+          <span className="text-[#71C9CE] font-extrabold drop-shadow-lg">
+            Student
+          </span>
+        </h1>
+        <Link to="/subject">
+          <button className="bg-white mt-5 hover:bg-gray-500 hover:text-white shadow-lg shadow-slate-700 text-gray-800 font-semibold w-[10rem] sm:w-[12rem] h-12 sm:h-14 py-2 px-4 border border-gray-400 rounded-xl">
+            Subjects
+          </button>
+        </Link>
+      </div>
+    </div>
 
-                    <div className="order-1 md:order-2 centered-earth">
-                        <div className="md:p-0 md:pt-00 text-center md:text-left">
-                            <div id="earth-container">
-                                <Earth className="shadow-xl" /> {/* Add the shadow class here */}
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </main>
+    <div className="order-1 md:order-2 centered-earth">
+      <Earth />
+    </div>
+  </div>
+</main>
+
 
 
 
