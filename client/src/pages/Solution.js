3c459a781a9b89791b603a8c077b88ac17223167
prince saspara <princesaspara46@gmail.com>
2023-12-10 12:13:59 +0530

    add output downloader

--- client/src/pages/Solution.js
+++ client/src/pages/Solution.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
@@ -8,22 +8,22 @@ import Footer from '../components/Footer';
 import Loader from '../components/Spinner';
 import NotFound from '../components/NotFound';
 import { Helmet } from 'react-helmet';
-import { faMoon } from '@fortawesome/free-solid-svg-icons';
-import { faSun } from '@fortawesome/free-solid-svg-icons';
-import { faCopy } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun, faCopy, faDownload } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import '../App.css'
+import html2canvas from 'html2canvas'; // Import html2canvas
+
+
+import '../App.css';
 
 const Solution = () => {
   const { practicalId } = useParams();
   const [practical, setPractical] = useState(null);
   const [solutions, setSolutions] = useState([]);
-  const [isCopied, setIsCopied] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
   const [outputBackgroundBlack, setOutputBackgroundBlack] = useState(false);
+  const outputRef = useRef(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     axios
@@ -31,12 +31,12 @@ const Solution = () => {
       .then((response) => {
         setPractical(response.data);
         setSolutions(response.data.solutions);
-        setLoading(false);
       })
       .catch((error) => {
         console.error('Error fetching solutions:', error);
-        setLoading(false);
-        setError('Error fetching solutions. Please try again later.');
+      })
+      .finally(() => {
+        setLoading(false); // Set loading to false regardless of success or failure
       });
   }, [practicalId]);
 
@@ -48,33 +48,40 @@ const Solution = () => {
     document.execCommand('copy');
     document.body.removeChild(tempTextArea);
 
-    setIsCopied(true);
-
     toast.success('Code copied to clipboard!', {
       position: 'top-center',
       autoClose: 2000,
     });
-
-    setTimeout(() => {
-      setIsCopied(false);
-    }, 2000);
   };
 
   const toggleOutputBackgroundColor = () => {
     setOutputBackgroundBlack(!outputBackgroundBlack);
   };
 
+  const downloadImage = () => {
+    const outputElement = outputRef.current;
+
+    html2canvas(outputElement).then((canvas) => {
+      const link = document.createElement('a');
+      link.href = canvas.toDataURL('image/png');
+      link.download = 'output_image.png';
+      link.click();
+    });
+
+    toast.success('Image downloaded!', {
+      position: 'top-center',
+      autoClose: 2000,
+    });
+  };
+
+  if (!practical || solutions.length === 0) {
+    return <NotFound />;
+  }
+
   if (loading) {
     return <Loader />;
   }
 
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (solutions.length === 0) {
-    return <NotFound />;
-  }
 
   return (
     <>
@@ -85,11 +92,9 @@ const Solution = () => {
       </Helmet>
       <Navbar />
       <div className="container mx-auto p-4">
-        {practical && (
-          <div className="bg-white p-4 rounded-lg shadow-md mb-4 Aim-hover">
-            <h3 className="text-lg font-medium mb-4">Topic: {practical.aim}</h3>
-          </div>
-        )}
+        <div className="bg-white p-4 rounded-lg shadow-md mb-4 Aim-hover">
+          <h3 className="text-lg font-medium mb-4">Topic: {practical.aim}</h3>
+        </div>
         {solutions.map((solution) => (
           <div key={solution._id} className="bg-white p-4 rounded-lg shadow-md mb-4">
             {solution.solutionCode && solution.solutionCode.length > 0 && (
@@ -110,22 +115,37 @@ const Solution = () => {
             )}
 
             {solution.codeOutput && solution.codeOutput.length > 0 && (
-              <div className="relative">
-                <strong>Output:</strong>
-                <button
-                  onClick={toggleOutputBackgroundColor}
-                  className="absolute top-2 right-2 p-1 rounded cursor-pointer icon fa-xl"
+              <div>
+                <div className="relative">
+                  <strong>Output:</strong>
+                  <button
+                    onClick={toggleOutputBackgroundColor}
+                    className="absolute top-2 right-2 p-1 rounded cursor-pointer icon fa-xl"
+                  >
+                    {outputBackgroundBlack ? <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}
+                  </button>
+                </div>
+                <div
+                  ref={outputRef}
+                  className={`mt-4 ${outputBackgroundBlack ? 'bg-black border-black text-white' : 'bg-white'}`}
                 >
-                  {outputBackgroundBlack ? <FontAwesomeIcon icon={faMoon} /> : <FontAwesomeIcon icon={faSun} />}
-                </button>
+                  {solution.codeOutput && solution.codeOutput.length > 0 && (
+                    <pre className="p-4 border rounded-lg whitespace-pre-wrap">{solution.codeOutput}</pre>
+                  )}
+
+                </div>
+                <button
+              onClick={downloadImage}
+              className="mt-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              <FontAwesomeIcon icon={faDownload} className="mr-2" />
+              Download Image
+            </button>
               </div>
             )}
 
-            <div className={`mt-4 ${outputBackgroundBlack ? 'bg-black text-white' : 'bg-white'}`}>
-              {solution.codeOutput && solution.codeOutput.length > 0 && (
-                <pre className="p-4 border rounded-lg whitespace-pre-wrap">{solution.codeOutput}</pre>
-              )}
-            </div>
+
+        
 
             {solution.explanation && solution.explanation.length > 0 && (
               <div className="mt-4">
